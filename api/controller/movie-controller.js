aa4232d7c8709bbdcbc0c716bd9961c256e962da
Jools Arts <jools.arts@gmail.com>
2022-05-18 09:51:17 +0100

    Developed a method for deleting a movie from a database and implemented it within the movie-controller.js file

--- api/controller/movie-controller.js
+++ api/controller/movie-controller.js
@@ -29,6 +29,24 @@ module.exports = {
     },
 
     updateMovie: async (req, res, next) => {
-        const id = req.params
+        const id = req.params.id;
+        const updates = req.body;
+        const movie = await Movie.updateOne({_id: id}, updates);
+
+        if (movie) {
+            res.status(200).json(movie);
+            return;
+        }
+        next(new MovieNotFoundError(id));
+    },
+
+    deleteMovie: async (req, res, next) => {
+        const movie = movies.find((movie) => movie.id === Number(req.params.id))
+
+        if(!movie) {
+        return res.status(404).json({success: false, msg: `The movie with the id you requested could not be found`})
+        }
+        const newMovies = movies.filter((movie) => movie.id !== Number(req.params.id))
+        return res.status(200).json({success: true, data: newMovies})
     }
 }
