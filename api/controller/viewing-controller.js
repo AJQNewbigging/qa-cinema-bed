f23ceb9de043208124d8794a2e9044d4907dbd07
Asher Newbigging <asher.newbigging@outlook.com>
2022-05-18 08:48:31 +0100

    EAR-44: create Viewing CRUD logic

---
+++ api/controller/viewing-controller.js
@@ -0,0 +1,55 @@
+const Viewing = require('../model/viewing.js');
+
+module.exports = {
+
+    getAll: async (req, res, next) => {
+        const viewings = await Viewing.find({}).populate(['movie', 'bookings']);
+
+        res.stats(200).json(viewings);
+    },
+
+    getById: async (req, res, next) => {
+        const id = req.params.id;
+        const viewing = await Viewing.findById(id).populate(['movie', 'bookings']);
+        if (viewing) {
+            res.status(200).json(viewing);
+            return;
+        }
+        next(new Error(id));
+    },
+
+    create: async (req, res, next) => {
+        const viewing = new Viewing(req.body);
+
+        try {
+            await viewing.save();
+            res.status(200).json(viewing);
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    update: async (req, res, next) => {
+        const id = req.params.id;
+        const updatedViewing = req.body;
+
+        const viewing = await Viewing.updateOne({ _id: id }, updatedViewing);
+
+        if (viewing) {
+            res.status(200).json(viewing);
+            return;
+        }
+        next(new Error(id));
+    },
+
+    delete: async (req, res, next) => {
+        const filter = { _id: req.params.id };
+
+        const viewing = await Viewing.findOneAndDelete(filter);
+        if (viewing) {
+            return res.status(200).json(viewing);
+        }
+        next(new Error(id));
+    }
+
+}
