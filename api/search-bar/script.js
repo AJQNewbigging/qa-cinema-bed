adb170a4d27fe78de49e3c720f5d2f4d8c53752e
Jools Arts <jools.arts@gmail.com>
2022-05-19 17:22:06 +0100

    Implemented a navbar element, although this is not currently integrated into the larger site

---
+++ api/search-bar/script.js
@@ -0,0 +1,26 @@
+const movieCardTemplate = document.querySelector("[data-movie-template]")
+const movieCardContainer = document.querySelector("[data-movie-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let movies = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.lowerCase()
+    movies.forEach(movie => {
+        const isVisible = movie.name.toLowerCase().includes(value) || movie.genre.toLowerCase().includes(value)
+        movie.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+// fetch(*URL*)
+//     .then(res).json())
+//     .then(data => {})
+//     movie = data.map(movie) => {
+//         const card = movieCardTemplate.content.cloneNode(true).children[0]
+//         const header = card.querySelector("[data-header")
+//         const body = card.querySelector("[data-body")
+//         header.textContent = movie.name
+//         body.textContent = movie.genre
+//         movieCardContainer.append(card)
+//         return { name: movie.name, genre: movie.genre, element: card}
+//     })
