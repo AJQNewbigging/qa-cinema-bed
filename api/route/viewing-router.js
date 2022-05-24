0cba59dbbab7f064ccfc007be3bc55ff87383612
James1803 <jamesalmond60@gmail.com>
2022-05-24 12:07:16 +0100

    created addBooking

--- api/route/viewing-router.js
+++ api/route/viewing-router.js
@@ -8,5 +8,6 @@ router.get('/:id', viewingController.getById);
 router.post('/', viewingController.create);
 router.put('/:id', viewingController.update);
 router.delete('/:id', viewingController.delete);
+router.put('/:id/booking', viewingController.addBooking);
 
 module.exports = router;
